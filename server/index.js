c35e449cda3a8496df8c44e7607a9abd413a0f1b
Tyler Wendover <wendovertyler@gmail.com>
2022-10-03 12:21:39 -0400

    added forgot gitignore

--- server/index.js
+++ server/index.js
@@ -1,19 +1,15 @@
-const http = require('http');
+const express = require('express')
+const app = express()
 
 const hostname = '127.0.0.1'; //localhost
-const port = 3000; 
+const port = process.env.PORT || 3000; 
 
-const server = http.createServer((req, res) => {
-
+app.get('/', (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
+})
 
-});
-
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);//not a ' but a `
-  
 });
-
-const fatArrow = x => x * 2; 
